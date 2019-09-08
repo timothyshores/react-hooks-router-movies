5889ee80c7c74f8d8c1b0cf1674183033f45b5da
Timothy Shores <timothymshores@gmail.com>
2019-09-08 18:43:40 -0500

    Added /movies/:id route for Movie component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
 const App = () => {
 	const [savedList, setSavedList] = useState([]);
@@ -15,6 +16,7 @@ const App = () => {
 		<div>
 			<SavedList list={savedList} />
 			<Route path="/" component={MovieList} />
+			<Route path="/movies/:id" component={Movie} />
 		</div>
 	);
 };
